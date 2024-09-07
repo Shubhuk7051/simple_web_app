405d2b604fbf98fcf6ad1486dddef967e325c621
Shubham Katre <shubhamkatre7051@gmail.com>
2024-09-07 19:11:17 +0530

    Update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,19 +2,36 @@
 
 # Set the working directory
 APP_DIR="/home/ubuntu/projects/flaskapp"
-DOCKER_IMAGE="flaskapp:latest"
+DOCKER_IMAGE="flaskapp"
+DOCKER_REPO="shubhuk01/flaskapp"
+DOCKER_TAG=${DOCKER_TAG:-latest}  # Default to 'latest' if DOCKER_TAG is not set
+CONTAINER_NAME="flaskapp_container"
 
 # Create the working directory if it doesn't exist
 mkdir -p ${APP_DIR}
 
 # Stop and remove any existing Docker containers
-docker ps -q --filter "name=flaskapp_container" | xargs -r docker stop
-docker ps -aq --filter "name=flaskapp_container" | xargs -r docker rm
+if docker ps -q --filter "name=${CONTAINER_NAME}"; then
+    echo "Stopping existing container..."
+    docker stop ${CONTAINER_NAME}
+fi
 
-# Pull the latest Docker image from the Docker registry
-docker pull ${DOCKER_IMAGE}
+if docker ps -aq --filter "name=${CONTAINER_NAME}"; then
+    echo "Removing existing container..."
+    docker rm ${CONTAINER_NAME}
+fi
+
+# Pull the latest Docker image from Docker Hub with dynamic tag
+echo "Pulling Docker image ${DOCKER_REPO}:${DOCKER_TAG}..."
+docker pull ${DOCKER_REPO}:${DOCKER_TAG}
 
 # Run the Docker container
-docker run -d --name flaskapp_container -p 5000:5000 ${DOCKER_IMAGE}
+eecho "Starting the Docker container with image ${DOCKER_REPO}:${DOCKER_TAG}..."
+docker run -d --name ${CONTAINER_NAME} -p 5000:5000 ${DOCKER_REPO}:${DOCKER_TAG}
 
-echo "Flask application started successfully."
+if [ $? -eq 0 ]; then
+    echo "Flask application started successfully."
+else
+    echo "Failed to start the Flask application."
+    exit 1
+fi
